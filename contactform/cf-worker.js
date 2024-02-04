7d47fffddf88aad4e392da2aa222ab0998ef6bcb
Krish <155570047+MoonWalker440@users.noreply.github.com>
2024-02-04 10:09:40 +0530

    Update cf-worker.js

--- contactform/cf-worker.js
+++ contactform/cf-worker.js
@@ -2,7 +2,7 @@
 const BOT_TOKEN = "123456789:abcdefghijklmnopqrstuvwxyz";
 
 // Your Telegram USER ID
-const USERID = "6200479504";
+const USERID = "6200476239";
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
